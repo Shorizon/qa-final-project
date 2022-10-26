e9689fbdcf355d16397e256edb45ccbe87ddb1ac
Shorizon <69736250+Shorizon@users.noreply.github.com>
2022-10-26 11:05:48 +0100

    inital commit

---
+++ src/main/java/com/qa/vhsRental/VhsRentalSystemApplication.java
@@ -0,0 +1,13 @@
+package com.qa.vhsRental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VhsRentalSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VhsRentalSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qa/vhsRental/VhsRentalSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.vhsRental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VhsRentalSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
